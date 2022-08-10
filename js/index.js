7d2f90a68863c0a26621f256e07f5b322974d65b
Lucas Vitro <lucasvitro9@gmail.com>
2022-08-10 12:08:08 -0400

    save 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
 // Iteration 1: Names and Input
-//
+
+let driver
+driver = "hacker1"
+let navigator 
+navigator = "aacker2"
+console.log (`The driver's name is ${driver}`)
+
 // Iteration 2: Conditionals
 
+if (driver.length > navigator.length){
+    console.log (`The driver has the longest name, it has ${driver.length } characters`)
+} else if (navigator.length > driver.length){
+    console.log(`It seems that the navigator has the longest name, it has ${navigator.length }  characters.`)
+} else if (driver.length === navigator.length){
+    console.log(`Wow, you both have equally long names, ${driver.length } characters!`)
+}
 
 // Iteration 3: Loops
+let separado = driver.split("").join(" ")
+console.log(separado.toUpperCase())
+
+let inverso = driver.split("").reverse().join("")
+console.log(inverso)
+ 
+let names = [driver, navigator]
+for (let i=0; i<names; i++)
+
+names.sort()
+console.log(names)
